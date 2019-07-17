e63b449214056a2e7f5e5b2d5eeed41d6c2fa057
yu mitsuhori <ymbullseye@gmail.com>
2019-07-17 20:15:59 +0900

    クリック時仮の値を表示

--- app/src/main/java/jp/co/youmitsu/retrofitmocksampleapp/MainActivity.kt
+++ app/src/main/java/jp/co/youmitsu/retrofitmocksampleapp/MainActivity.kt
@@ -1,60 +1,92 @@
 package jp.co.youmitsu.retrofitmocksampleapp
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import jp.co.youmitsu.retrofitmocksampleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var adapter: RecyclerViewAdapter
 
     lateinit var repository: Repository
-
-    val listData: MutableList<ZipData> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         repository = Repository()
-        adapter = RecyclerViewAdapter(listData)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.list.adapter = adapter
+        binding.list.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = RecyclerViewAdapter(this@MainActivity)
+            addItemDecoration(
+                DividerItemDecoration(
+                    this@MainActivity,
+                    LinearLayoutManager(this@MainActivity).orientation
+                )
+            )
+        }
+        binding.handler = this
     }
 
     fun onClickSearch() {
         val inputZipCode = binding.editText.text.toString()
         repository.searchZipCode(inputZipCode, { data ->
-            listData.clear()
-            data.forEach {
-                listData.add(it)
+            (binding.list.adapter as? RecyclerViewAdapter)?.let {
+                it.updateData(data)
+                it.notifyDataSetChanged()
             }
-            adapter.notifyDataSetChanged()
         }, { message ->
         })
     }
 }
 
 class RecyclerViewAdapter(
-    val data: List<ZipData>
+    private val context: Context
 ) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private var data: List<ZipData> = mutableListOf()
+
+    fun updateData(newData: List<ZipData>) {
+        data = newData
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.recycler_item, parent, false)
+        return ViewHolder(view)
     }
+
+    override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        data[position].apply {
+            holder.address1.text = address1
+            holder.address2.text = address2
+            holder.address3.text = address3
+            holder.kana1.text = kana1
+            holder.kana2.text = kana2
+            holder.kana3.text = kana3
+            holder.zipcode.text = zipCode
+            holder.prefcode.text = prefCode
+        }
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val address1: TextView = view.findViewById(R.id.address1)
+        val address2: TextView = view.findViewById(R.id.address2)
+        val address3: TextView = view.findViewById(R.id.address3)
+        val kana1: TextView = view.findViewById(R.id.kana1)
+        val kana2: TextView = view.findViewById(R.id.kana2)
+        val kana3: TextView = view.findViewById(R.id.kana3)
+        val zipcode: TextView = view.findViewById(R.id.zipcode)
+        val prefcode: TextView = view.findViewById(R.id.prefcode)
     }
 }
--- app/src/main/java/jp/co/youmitsu/retrofitmocksampleapp/Repository.kt
+++ app/src/main/java/jp/co/youmitsu/retrofitmocksampleapp/Repository.kt
@@ -5,7 +5,14 @@ class Repository {
     fun searchZipCode(entry: String?, onSuccess: (result: List<ZipData>) -> Unit, onFailed: (message: String) -> Unit) {
         val result = listOf(
             ZipData(
-                "", "", "", "", "", "", "", ""
+                "hoge",
+                "hoge",
+                "hgoe",
+                "hoge",
+                "hgoe",
+                "hoge",
+                "1201011",
+                "ja-01"
             )
         )
         onSuccess(result)
